ac5ec3ef908dd7f41e0712203716db65729fb49f
petertrai <petertrai@gmail.com>
2023-08-25 14:27:37 -0700

    auth0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+
       <body className={cn(inter.className, "antialiased min-h-screen pt-16")}>{children}</body>
+      
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Navbar from "@/components/Navbar"
 import { prisma } from "@/lib/db"
 
 export default function Home() {
   return (
+    <div>
+    <Navbar />
     
       <h1 className="text-red-600">Hello worlds</h1>
+      </div>
       
   )
 }
