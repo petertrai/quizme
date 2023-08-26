5e7ab1caf9ab06bbfb6b668d5c347f91150f2d01
petertrai <petertrai@gmail.com>
2023-08-26 11:43:50 -0700

    Form

--- src/components/QuizCreation.tsx
+++ src/components/QuizCreation.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React from "react";
 import {
@@ -8,12 +8,39 @@ import {
   CardHeader,
   CardTitle,
 } from "./ui/card";
-import QuizCreationForm from "./QuizCreationForm";
-
+import { useForm } from "react-hook-form";
+import { quizCreationSchema } from "../../schemas/form/quiz";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
 type Props = {};
 
+type Input = z.infer<typeof quizCreationSchema>;
+
 const QuizCreation = (props: Props) => {
+  const form = useForm<Input>({
+    resolver: zodResolver(quizCreationSchema),
+    defaultValues: {
+      amount: 3,
+      topic: "",
+      type: "mcq",
+    },
+  });
+
+  function onSubmit(input: Input) {
+    alert(JSON.stringify(input, null, 2));
+  }
 
   return (
     <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
@@ -23,7 +50,27 @@ const QuizCreation = (props: Props) => {
           <CardDescription>Choose a topic</CardDescription>
         </CardHeader>
         <CardContent>
-         <QuizCreationForm />
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="topic"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Topic</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is what you will be quizzed on.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
         </CardContent>
       </Card>
     </div>
